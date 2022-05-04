bd8cece8d4f0000db0a037b8f9ef7cf61d52c2b7
Erick Ocampo <Erickocampocortez@gmail.com>
2022-05-04 14:45:23 -0500

    cambios al gitIgnore

--- src/main/java/com/erick/boutique/controller/ProductoController.java
+++ src/main/java/com/erick/boutique/controller/ProductoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"*"})
+@CrossOrigin(origins = {"http://localhost:4200/"})
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
